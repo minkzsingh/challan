61de1a63e52280ec39b3c2a51c461f5087894447
Gurinder Singh <gurindersingh.dev@gmail.com>
2020-04-05 00:20:00 +0530

    get column fix

--- public/js/datatable_service.js
+++ public/js/datatable_service.js
@@ -2,7 +2,6 @@ function getTable(params) {
 
     var table = $(params.table_id).DataTable({
         bScrollCollapse: true,
-        bJQueryUI: true,
         aoColumnDefs: [{
             sWidth: "16%",
             aTargets: [-1]
@@ -11,23 +10,37 @@ function getTable(params) {
             url: params.url,
             data: params.data
         },
-        columns: [{
-                data: "id"
-            },
-            {
-                data: "name"
-            },
-            {
-                data: "quantity"
-            },
-            {
-                mRender: function(data, type, row) {
-                    return '<button type="button" class="btn btn-primary edit" style="width: 60px;margin-right:10px;">Edit</button><button type="button" class="btn btn-danger delete" style="width: 80px;">Delete</button>';
-                }
-            }
-        ]
+        columns: getColumns(params.url)
 
     });
 
     return table;
+}
+
+function getColumns(url) {
+    var col = [];
+    switch (url) {
+        case 'api/item':
+            col = [{
+                    data: "id"
+                },
+                {
+                    data: "name"
+                },
+                {
+                    data: "quantity"
+                }
+            ]
+            break;
+
+        default:
+            break;
+    }
+    if (col.length > 0) {
+        col.push({
+            defaultContent: '<button type="button" class="btn btn-primary edit" style="width: 60px;margin-right:10px;">Edit</button><button type="button" class="btn btn-danger delete" style="width: 80px;">Delete</button>'
+        });
+    }
+
+    return col;
 }
